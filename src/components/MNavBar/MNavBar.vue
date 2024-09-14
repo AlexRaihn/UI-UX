fb1ebcab338d8c245c111925015486fb1be0de07
AlexRaihn <iogan.dead@yandex.ru>
2024-09-15 02:58:58 +0300

    [dev-1]/test

--- src/components/MNavBar/MNavBar.vue
+++ src/components/MNavBar/MNavBar.vue
@@ -1,70 +1,79 @@
 <template>
-      <div
-          class="w-full flex items-center gap-4 py-4 px-8 max-sm:flex-wrap" :class="[position, bg, color]"
-      >
-          <div class="flex items-center gap-4" :class="[linksPosition]">
-            <slot name="logo"/>
-          </div>
+  <div
+    class="w-full flex items-center gap-4 py-4 px-8 max-sm:flex-wrap"
+    :class="[position, bg, color]"
+  >
+    <div class="flex items-center gap-4" :class="[linksPosition]">
+      <slot name="logo" />
+    </div>
 
-          <div class="flex items-center gap-4 max-sm:hidden" :class="[linksPosition]">
-                <slot name="links"/>
-          </div>
-          <div class="flex items-center gap-4 max-sm:hidden" :class="[linksPosition]">
-            <slot name="login"/>
-          </div>
+    <div class="flex items-center gap-4 max-sm:hidden" :class="[linksPosition]">
+      <slot name="links" />
+    </div>
+    <div class="flex items-center gap-4 max-sm:hidden" :class="[linksPosition]">
+      <slot name="login" />
+    </div>
 
-          <div  class="hidden max-sm:block">
-            <i class="fa-solid fa-bars duration-300" :class="{'rotate-90' : isCollapsed}" @click="toggleCollapse"></i>
-          </div>
-          <div :class="{'animate__animated animate__fadeInDown' : isCollapsed}" class=" w-full shrink sm:hidden" v-if="isCollapsed">
-            <div class="flex flex-col gap-4">
-              <div>
-                <slot name="links"/>
-              </div>
-              <div>
-                <slot name="login"/>
-              </div>
-            </div>
-          </div>
+    <div class="hidden max-sm:block">
+      <i
+        class="fa-solid fa-bars duration-300"
+        :class="{ 'rotate-90': isCollapsed }"
+        @click="toggleCollapse"
+      ></i>
+    </div>
+    <div
+      :class="{ 'animate__animated animate__fadeInDown': isCollapsed }"
+      class="w-full shrink sm:hidden"
+      v-if="isCollapsed"
+    >
+      <div class="flex flex-col gap-4">
+        <div>
+          <slot name="links" />
+        </div>
+        <div>
+          <slot name="login" />
+        </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'NavBar',
-    data() {
-        return {
-          isCollapsed: false,
-        }
+  name: "NavBar",
+  data() {
+    return {
+      isCollapsed: false,
+    };
+  },
+  props: {
+    position: {
+      type: String,
+      required: false,
+      default: "justify-between",
     },
-    props: {
-        position: {
-            type: String,
-            required: false,
-            default: 'justify-between'
-        },
-        linksPosition: {
-            type: String,
-            required: false,
-            default: 'justify-between'
-        },
-        bg: {
-            type: String,
-            required: false,
-            default: 'bg-gray-500'
-        },
-        color: {
-            type: String,
-            required: false,
-            default: 'text-black'
-        }
+    linksPosition: {
+      type: String,
+      required: false,
+      default: "justify-between",
     },
-    methods: {
-        toggleCollapse() {
-          this.isCollapsed = !this.isCollapsed;
-        }
-    }
+    bg: {
+      type: String,
+      required: false,
+      default: "bg-gray-500",
+    },
+    color: {
+      type: String,
+      required: false,
+      default: "text-black",
+    },
+  },
+  methods: {
+    toggleCollapse() {
+      this.isCollapsed = !this.isCollapsed;
+    },
+  },
 });
 </script>
